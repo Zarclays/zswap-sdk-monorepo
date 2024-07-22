a05b39dd1f6ac09b8d791cfe9d7b8def28ca8f3c
layinka <xcelsis02@gmail.com>
2024-07-22 11:13:54 +0100

    throw error if init_code_hash not set

--- packages/core-sdk/src/functions/computePairAddress.ts
+++ packages/core-sdk/src/functions/computePairAddress.ts
@@ -13,7 +13,11 @@ export const computePairAddress = ({
   tokenA: Token
   tokenB: Token
 }): string => {
+  
   const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
+  if(!INIT_CODE_HASH[token0.chainId]){
+    throw new Error(`INIT CODE HASH NOT Set for ${token0.chainId}`)
+  }
   return getCreate2Address(
     factoryAddress,
     keccak256(['bytes'], [pack(['address', 'address'], [token0.address, token1.address])]),
